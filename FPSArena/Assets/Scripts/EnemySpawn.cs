663627763b421cf6807e23abb4f90f28b13b6af1
Jhisscock <jthisscock@mail.lipscomb.edu>
2019-10-29 10:36:40 -0500

    Jacob Commit

---
+++ FPSArena/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject [] spawnPoints = new GameObject [4];
+    private float timer = 2f;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if(timer >= 3){
+            timer = 0f;
+            Instantiate(enemy,spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position, Quaternion.identity);
+        }
+        
+    }
+}
